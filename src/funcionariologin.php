0b71b0b0873b7a3b0ed12fe1448b90ac3fa3b2a1
dsmachine <ds102turma@gmail.com>
2022-10-06 15:38:07 -0300

    cliente editar a senha e critografia de senha

---
+++ src/funcionariologin.php
@@ -0,0 +1,32 @@
+<?php
+    // Recebe os campos do formulário
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+
+    // Realiza a consulta no banco de dados
+    include "conectauser.inc";
+    $sql = "SELECT * FROM func WHERE email = '$email';";
+    $res = mysqli_query($mysqli, $sql);
+
+    //testa se não encontrou o e-mail
+    if(mysqli_num_rows($res) != 1){
+        echo "Senha e/ou Email inválido(s)!";
+        echo "<p><a href='funcionariologin.html'>Página de login</a></p>";
+    }
+    else{
+        $func = mysqli_fetch_array($res);
+        // testa se a senha está errada
+        if($senha != $func["senha"]){
+            echo "Senha e/ou Email inválido(s)!";
+            echo "<p><a href='funcionariologin.html'>Página de login</a></p>";
+        }
+        else{
+            // abre a sessão e registra as variáveis do login
+            session_start();
+            $_SESSION["email"] = $email;
+            $_SESSION["senha"] = $senha;
+            // direciona para a página inicial
+            header("Location: index.php");
+        }
+    }
+?>
