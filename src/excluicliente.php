052b3b1ed4352a2b3689f6661f41a967d119b926
Raphael <raphaelormonde@gmail.com>
2022-12-08 14:38:47 -0300

    exclui cliente (não completo)

--- src/excluicliente.php
+++ src/excluicliente.php
@@ -1,4 +1,18 @@
-<?php  include "autentica.php";
-"DELETE FROM `cliente` WHERE `cliente`.`email` = $email";
-header("Location: index.php");
+<?php  include "conectauser.inc";
+
+session_start();
+if(isset($_SESSION["email"])){
+    $email = $_SESSION["email"];
+}
+
+
+$sql ="DELETE FROM `cliente` WHERE `cliente`.`email` = $email";
+if(mysqli_query($mysqli, $sql)){
+    include "envia_email.php";
+    envia_email($email, "Exclusão de Conta", "Sua conta foi excluída do site da hotelaria canina Fenrir Pet Shop.");
+    header('location: index.php');
+}
+header('location: index.php');
+header('location:index.php');
 ?>
+<?php mysqli_close($mysqli); ?>
