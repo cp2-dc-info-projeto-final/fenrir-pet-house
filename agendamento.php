a9b92f3fede6007e2526c092bdb27b4d982804dd
Thi-casa <thiagosilvade06@gmail.com>
2022-12-11 18:06:48 -0300

    php de agendamento (INCOMPLETO)

---
+++ agendamento.php
@@ -0,0 +1,15 @@
+<?php
+
+include "conectauser.inc";
+
+$sql = "SELECT * FROM servico WHERE agenda_status = '0' ;"; 
+$res = mysqli_query($mysqli,$sql);
+$linhas = mysqli_num_rows($res);
+for($i = 0; $i < $linhas; $i++){
+    $servico = mysqli_fetch_array($res);
+    echo "Reservas Disponíveis nesse Horário: ".$servico["hora_disp"]."<br>";
+    echo "Horário: ".$servico["agenda_tstamp"]."<br>";     
+    echo "---------------------<br>";
+}
+
+?>
