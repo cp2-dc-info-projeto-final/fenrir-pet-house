acfcb466b45df5dcbb5d3e973d8df75da46cfd3e
Raphael <raphaelormonde@gmail.com>
2022-12-12 14:46:08 -0300

    exclusão de conta de cliente

--- src/excluicliente.php
+++ src/excluicliente.php
@@ -1,18 +1,24 @@
 <?php  include "conectauser.inc";
 
-session_start();
-if(isset($_SESSION["email"])){
-    $email = $_SESSION["email"];
-}
+$email = $_POST["email"];
+$senha = $_POST["senha"];
 
 
-$sql ="DELETE FROM `cliente` WHERE `cliente`.`email` = $email";
-if(mysqli_query($mysqli, $sql)){
-    include "envia_email.php";
-    envia_email($email, "Exclusão de Conta", "Sua conta foi excluída do site da hotelaria canina Fenrir Pet Shop.");
-    header('location: index.php');
-}
+//pega info "senha" do sql
+$sql = "SELECT senha FROM cliente WHERE email = '$email' ";
+$query = $mysqli->query($sql);
+$row = $query->fetch_assoc();
+
+//checa senha
+if(password_verify($senha, $row['senha'])){
+    //deleta conta
+    $sql2 ="DELETE FROM cliente WHERE email = '$email' ";
+        if(mysqli_query($mysqli, $sql2)){
+            include "envia_email.php";
+            envia_email($email, "Sentiremos sua Falta!", "Sua conta foi excluída do site da hotelaria canina Fenrir Pet Shop. Esperamos que você e seu au-migo tiveram a melhor experiência.");
+            header('location: index.php');
+        }
+    }
 header('location: index.php');
-header('location:index.php');
 ?>
 <?php mysqli_close($mysqli); ?>
