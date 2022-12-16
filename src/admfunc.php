c97b7cdce2c4ea99ef88aa74869995eb408ac935
Raphael <raphaelormonde@gmail.com>
2022-12-16 16:27:25 -0300

    adm edita funcionário (incompleto)

--- src/admfunc.php
+++ src/admfunc.php
@@ -1,16 +1,127 @@
 <?php
-include "conectauser.inc";
+    $operacao = $_POST["operacao"];
+    include "conectauser.inc";
 
-$sql = "SELECT * FROM func;";
-$res = mysqli_query($mysqli,$sql);
-$linhas = mysqli_num_rows($res);
-for($i=0; $i < $linhas; $i++){
-    $func = mysqli_fetch_array($res);
-    echo "Senha: ".$func["senha"]."<br>";
-    echo "Nome: ".$func["nome"]."<br>";
-    echo "Email: ".$func["email"]."<br>";
-    echo "Data de Nascimento: ".$func["data_nasc"]."<br>";
-    echo "CPF: ".$func["cpf"]."<br>";
-    echo "----------------------------------<br>";
-}
+    if($operacao == "inserir"){
+        $senha = $_POST["senha"];
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $csenha = ["csenha"];
+        $cpf = ["cpf"]
+        $data_nasc = ["data_nasc"];
+        $erro = 0;
+
+        if(empty($nome) or strstr($nome, ' ') == false){
+            echo "Por favor, preencha o nome completo.<br>";
+            $erro = 1;
+            
+        }
+
+        if(strlen($email) < 10 or strstr($email, '@') == false){
+            echo "Por favor, preencha o e-mail corretamente.<br>";
+            $erro = 1;            
+        }
+
+        if(empty($data_nasc)){
+            echo "Por favor, preencha a data.<br>";
+            $erro = 1;           
+        }
+        
+        if(empty($csenha)){
+            echo "Por favor, confirme a senha.<br>";
+            $erro = 1;
+           
+        }
+
+        if(empty($cpf)){
+            echo "Por favor, preencha o cpf<br>";
+            $erro = 1;
+        }
+
+        if(empty($senha)){
+            echo "Por favor, preencha a senha<br>";
+            $erro = 1;            
+            
+        }
+        if($erro == 0){
+            include "envia_email.php";
+            $hash = password_hash($senha, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO func (nome,senha,email,data_nasc)";
+            $sql .= "VALUES ('$nome','$hash','$email','$data_nasc');"; 
+
+            mysqli_query($mysqli,$sql);
+            envia_email($email, "Confirmação de Cadastro", "Parabéns $nome, sua conta foi criada na hospedaria canina Fenrir Pet House usando esse email. Esperamos que você e seu au-migo desfrutem de nossos serviços! Caso você não tenha feito essa conta, contate-nos imediatamente.");
+        }
+        
+    }
+    elseif($operacao == "exibir"){
+        $sql = "SELECT * FROM func;";
+        $res = mysqli_query($mysqli,$sql);
+        $linhas = mysqli_num_rows($res);
+        for($i=0; $i < $linhas; $i++){
+            $func = mysqli_fetch_array($res);
+            echo "Senha: ".$func["senha"]."<br>";
+            echo "Nome: ".$func["nome"]."<br>";
+            echo "Email: ".$func["email"]."<br>";
+            echo "<a href='altera.php?cod_func=".$func["cod_func"]."'>Alterar usuário incompleto</a><br>";
+            echo "----------------------------------<br>";
+        }
+    }
+    elseif($operacao == "buscar"){
+        $nome = $_POST["nome"];
+        $sql = "SELECT * FROM func WHERE nome like '%$nome%';";
+        $res = mysqli_query($mysqli,$sql);
+        $linhas = mysqli_num_rows($res);
+        for($i=0; $i < $linhas; $i++){
+            $func = mysqli_fetch_array($res);
+            echo "Senha: ".$func["senha"]."<br>";
+            echo "Nome: ".$func["nome"]."<br>";
+            echo "Email: ".$func["email"]."<br>";
+            echo "----------------------------------<br>";
+        }
+    }
+    elseif($operacao == "atualizar"){
+        $senha = $_POST["senha"];
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $erro = 0;
+
+        if(empty($nome) or strstr($nome, ' ') == false){
+            echo "Por favor, preencha o nome completo.<br>";
+            $erro = 1;
+            
+        }
+
+        if(strlen($email) < 10 or strstr($email, '@') == false){
+            echo "Por favor, preencha o e-mail corretamente.<br>";
+            $erro = 1;            
+        }
+
+        if(empty($data_nasc)){
+            echo "Por favor, preencha a data.<br>";
+            $erro = 1;           
+        }
+        
+        if(empty($csenha)){
+            echo "Por favor, confirme a senha.<br>";
+            $erro = 1;
+           
+        }
+
+        if(empty($senha)){
+            echo "Por favor, preencha a senha<br>";
+            $erro = 1;            
+            
+        }
+        // VERIFICA SE NÃO HOUVE ERRO 
+        if($erro == 0) {
+            $sql = "UPDATE func SET nome = '$nome', senha = '$senha',";
+            $sql .= "nome = '$nome', email = '$email' "; 
+            $sql .= "WHERE cod_func = $cod_func;";
+            mysqli_query($mysqli,$sql);  
+            echo "<br>O usuário foi atualizado com sucesso!";
+            echo "<br><a href='adminpage.php'>Página Inicial</a>";
+        }
+    }
+    mysqli_close($mysqli);
 ?>
